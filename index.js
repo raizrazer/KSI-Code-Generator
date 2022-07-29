885e54582e15721a2a3d54960aa4b1a9c1c917d6
Raiz <41806230+raizrazer@users.noreply.github.com>
2022-07-30 00:27:28 +0530

    single added

--- index.js
+++ index.js
@@ -3,16 +3,110 @@
 // document.onload = (event) => {
 //   console.log('page is fully loaded');
 // };
+let single = document.getElementById("one-or-two");
 
 let calc_btn = document.getElementById("calc");
-  calc_btn.addEventListener("click",()=>{
-    calc();
-    console.log("Calculated");
-  })
+calc_btn.addEventListener("click", () => {
+  if (single.checked) {
+    calcSingle();
+    console.log("Single Calculated");
+  } else {
+    calcDouble();
+    console.log("Double Calculated");
+  }
+});
 
-function calc() {
+function calcSingle() {
   let body = document.getElementById("main-center");
-  body.classList.remove("main-center")
+  body.classList.remove("main-center");
+  let howto = document.getElementById("howto");
+  howto.classList.add("hide");
+  let code = document.getElementById("code");
+  let st = code.value.toUpperCase();
+  console.log(st);
+  document.getElementById("app").innerHTML = `
+  <h1 style="padding:10px;margin-bottom:10px;">${st}<span style="color:rgb(17, 94, 156);;">XX</span></h1>
+  `;
+  var char = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
+  ];
+  var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var charlen = char.length;
+  var numlen = num.length;
+  var arr = [];
+
+  for (let i = 0; i < charlen; i++) {
+    arr.push(st + char[i]);
+  }
+
+  for (let i = 0; i < numlen; i++) {
+    arr.push(st + num[i]);
+  }
+
+  // console.log(arr)
+  // document.getElementById("table").innerHTML = `<td class="item">${arr}</td>`;
+  let mainlen = arr.length;
+  let table = document.getElementById("table");
+  table.textContent = "";
+  for (let i = 0; i < mainlen; i++) {
+    let text = document.createElement("div");
+    text.setAttribute("id", `${i.toString()}`);
+    text.setAttribute("class", `item`);
+    text.setAttribute("done", "");
+    text.textContent = `${arr[i]}`;
+    table.append(text);
+    // console.log(text)
+  }
+
+  for (let i = 0; i < mainlen; i++) {
+    let item = document.getElementById(i.toString());
+    item.addEventListener("click", (v) => {
+      let done = item.getAttribute("done");
+      if (done == "" || done == "false") {
+        console.log("false clicked");
+        item.setAttribute("done", "true");
+      } else if (done == "true") {
+        console.log("true clicked");
+        item.setAttribute("done", "false");
+      }
+      let id = v.target.id.toString();
+      var copyText = document.getElementById(id);
+      let text = copyText.innerText;
+      console.log(text);
+      navigator.clipboard.writeText(text);
+      // alert("Copied the text: " + text);
+    });
+  }
+}
+
+function calcDouble() {
+  let body = document.getElementById("main-center");
+  body.classList.remove("main-center");
   let howto = document.getElementById("howto");
   howto.classList.add("hide");
   let code = document.getElementById("code");
@@ -47,7 +141,7 @@ function calc() {
     "W",
     "X",
     "Y",
-    "Z",
+    "Z"
   ];
   var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var charlen = char.length;
@@ -90,14 +184,13 @@ function calc() {
   for (let i = 0; i < mainlen; i++) {
     let item = document.getElementById(i.toString());
     item.addEventListener("click", (v) => {
-    let done = item.getAttribute("done")
-      if(done == "" || done == "false"){
+      let done = item.getAttribute("done");
+      if (done == "" || done == "false") {
         console.log("false clicked");
-        item.setAttribute("done","true");
-      }
-      else if(done == "true"){
+        item.setAttribute("done", "true");
+      } else if (done == "true") {
         console.log("true clicked");
-        item.setAttribute("done","false")
+        item.setAttribute("done", "false");
       }
       let id = v.target.id.toString();
       var copyText = document.getElementById(id);
